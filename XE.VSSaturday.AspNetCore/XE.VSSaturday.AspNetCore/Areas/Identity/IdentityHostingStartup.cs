0dc1f584515f5f9199ba0b4835187359c34ee582
Andrea Dottor <andrea@dottor.net>
2018-11-10 16:53:14 +0100

    Upload XE.VSSaturday.AspNetCore

---
+++ XE.VSSaturday.AspNetCore/XE.VSSaturday.AspNetCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using XE.VSSaturday.AspNetCore.Data;
+
+[assembly: HostingStartup(typeof(XE.VSSaturday.AspNetCore.Areas.Identity.IdentityHostingStartup))]
+namespace XE.VSSaturday.AspNetCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ XE.VSSaturday.AspNetCore/XE.VSSaturday.AspNetCore/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XE.VSSaturday.AspNetCore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            //HttpContext.Response.Cookies.Append(
+            //        "name", 
+            //        "Andrea", 
+            //        new Microsoft.AspNetCore.Http.CookieOptions() 
+            //        { 
+            //            IsEssential = true 
+            //        });
+        }
+    }
+}
